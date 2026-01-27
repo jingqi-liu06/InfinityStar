e7968c6287732ae585c7c5c756d9efa9b7f24106
jingqi-liu06 <jingqi.liu03@outlook.com>
2026-01-27 15:10:05 +0800

    [feat] Modify inference and training scripts for subject-specific processing; update CUDA device, raw EEG paths, and output directories to accommodate new subjects and enhance batch processing.

--- scripts/infer_alig_self_encoder.sh
+++ scripts/infer_alig_self_encoder.sh
@@ -1,6 +1,7 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 python infer_alignment.py \
-    --raw_eeg_path "./eeg_data/Preprocessing/Segmented_1000ms_sw/sub9.npy" \
+    --raw_eeg_path "./eeg_data/Preprocessing/Segmented_1000ms_sw/sub6.npy" \
     --encoder_model "glmnet" \
-    --checkpoint_path "./checkpoints_alignment/sub9_glmnet_raw/checkpoint_best.pth" \
-    --output_path "./eeg_glmnet_results/sub9_latents.pt"
+    --checkpoint_path "./checkpoints_alignment/checkpoint_best.pth" \
+    --output_path "./eeg_glmnet_results/sub6_latents.pt" \
+    --raw_stats_path "./checkpoints_alignment/sub6_stats.npz"
